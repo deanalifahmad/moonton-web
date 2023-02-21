1e47ef45fbe9f085f49491c919dc6f5f7ba97dc9
Dean Alif Ahmad <jalal8915@outlook.com>
2023-02-21 23:33:27 +0700

    feat: integration sign in

--- resources/js/Components/InputError.jsx
+++ resources/js/Components/InputError.jsx
@@ -1,3 +1,3 @@
 export default function InputError({ message, className = '' }) {
-    return message ? <p className={'text-sm text-red-600 ' + className}>{message}</p> : null;
+    return message ? <p className={'px-4 py-2 text-sm text-red-600 border-2 border-red-600 rounded-md ' + className}>{message}</p> : null;
 }
